cf486a2734ed53a47c1f303952b071a298c3562c
GGustavoGG <gustavosilvadky02@gmail.com>
2021-06-20 16:00:04 -0300

    Terminado função que calcula gonorreia, remodelado as funções de pertinência

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from random import random
+
 from diagnosticador.diagnosticador import Diagnosticador, Sintomas, LabelSintomas
+
+def mockaDados(sintomas):
+    sintomas.ultima_relacao_sexual_vaginal = 5
+    sintomas.ultima_relacao_sexual_oral = 5
+    sintomas.ultima_relacao_sexual_anal = 5
+
+    sintomas.coceira_genitalia = 60
+    sintomas.dor_durante_relacao_sexual = 20
+    sintomas.dor_urinar = 90
+    sintomas.corrimento_amarelado_claro = 60
+
 
 if __name__ == '__main__':
     print("Bem vindo")
     diagnosticador = Diagnosticador()
-    sintoms = Sintomas()
-    print(LabelSintomas.MUITO_BAIXA.value)
+    sintomas = Sintomas()
+    mockaDados(sintomas)
+    diagnosticador.diagnostico_gonorreia(sintomas)
+
