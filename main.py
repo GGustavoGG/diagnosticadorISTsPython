c1dfe4f58a2ccb6efe1cef761d491626a5cd232a
GGustavoGG <gustavosilvadky02@gmail.com>
2021-06-21 13:39:54 -0300

    Finalizado o diagnosticado da doença sifilis

--- main.py
+++ main.py
@@ -7,10 +7,27 @@ def mockaDados(sintomas):
     sintomas.ultima_relacao_sexual_oral = 5
     sintomas.ultima_relacao_sexual_anal = 5
 
+    sintomas.primeira_relacao_sexual_vaginal = 2500
+    sintomas.primeira_relacao_sexual_oral = 2500
+    sintomas.primeira_relacao_sexual_anal = 2500
+
     sintomas.coceira_genitalia = 60
     sintomas.dor_durante_relacao_sexual = 20
     sintomas.dor_urinar = 90
     sintomas.corrimento_amarelado_claro = 60
+
+    sintomas.ferida_genitalia = 70
+    sintomas.ingua_virilha = 80
+
+    sintomas.ferida_cicatrizada_genitalia = 70
+    sintomas.manchas_corpo = 80
+    sintomas.descamacao_pele = 90
+    sintomas.inguas_pelo_corpo = 80
+
+    sintomas.lesoes_pele = 100
+    sintomas.vomito = 100
+    sintomas.convulsoes = 100
+    sintomas.delirios = 80
 
 
 if __name__ == '__main__':
@@ -18,5 +35,8 @@ if __name__ == '__main__':
     diagnosticador = Diagnosticador()
     sintomas = Sintomas()
     mockaDados(sintomas)
-    diagnosticador.diagnostico_gonorreia(sintomas)
+    #diagnosticador.diagnostico_gonorreia(sintomas)
+    #diagnosticador.diagnostico_sifilis_estagio1(sintomas)
+    #diagnosticador.diagnostico_sifilis_estagio2(sintomas)
+    diagnosticador.diagnostico_sifilis_estagio3(sintomas)
 
